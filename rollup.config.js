f6f827d37edb76ba9bbb268909f0665bd92a81c7
Jon Swenson <jswenson74@gmail.com>
2018-06-09 00:39:50 -0400

    Updated dependencies and rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
